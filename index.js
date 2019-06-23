960cf01f55aa22cf2cc329969fcd203e546951e9
Audra Tallman <audi.tallman@gmail.com>
2019-06-23 22:49:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function driversWithRevenueOver()
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue;
+  });
+}
 
 function driverNamesWithRevenueOver()
 
